a9fd5f15c1dd3726f978bc6a5441034ba78e6229
mw-jsgeorge <73989764+mw-jsgeorge@users.noreply.github.com>
2024-02-29 10:55:03 +0530

    Create buildfile.m

---
+++ buildfile.m
@@ -0,0 +1,15 @@
+function plan = buildfile
+
+% Add the source folder to the path
+addpath("source");
+
+% Create a plan
+plan = buildplan(localfunctions);
+
+% Add a task to run tests
+plan("test") = matlab.buildtool.tasks.TestTask("tests");
+
+% Make the "test" task the default task in the plan
+plan.DefaultTasks = "test";
+
+end
