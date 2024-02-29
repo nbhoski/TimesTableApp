58628901d9c8a22b154651ec9d902859be92358c
mw-jsgeorge <73989764+mw-jsgeorge@users.noreply.github.com>
2024-02-29 11:26:08 +0530

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -9,7 +9,9 @@ plan = buildplan(localfunctions);
 % Add a task to run tests
 plan("test") = matlab.buildtool.tasks.TestTask("tests");
 
+plan("check") = CodeIssuesTask;
+
 % Make the "test" task the default task in the plan
-plan.DefaultTasks = "test";
+plan.DefaultTasks = "check";
 
 end
